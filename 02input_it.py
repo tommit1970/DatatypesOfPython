692b14a161cd3bec4f17328e85b8e933fe168b59
Tommy Taraldsvik <epleregime@gmail.com>
2020-03-31 20:21:09 +0000

    Initial commit

---
+++ 02input_it.py
@@ -0,0 +1,7 @@
+print("Hva heter du?")
+svar = input()
+
+print("Du heter",svar) # Komma lager mellomrom
+
+print("Du heter "+svar) # Pluss gjør det ikke
+
---
+++ main.py
@@ -0,0 +1,16 @@
+# types of variables
+
+number = 12
+myString = "A"
+pythonIsFun = True
+myFavoriteNerds = ["Einstein", "Hawkins"]
+myFavoriteColor = (0,255,0)
+myData = {"name":"Tommy Taraldsvik", "age": 49, "address":"Dreggveien 12"}
+
+
+print(type(number))
+print(type(myString))
+print(type(pythonIsFun))
+print(type(myFavoriteNerds))
+print(type(myFavoriteColor))
+print(type(myData))
